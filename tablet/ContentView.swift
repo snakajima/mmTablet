774776fe677c9b28374a0678a747e9a9a3d633e5
Satoshi Nakajima <satoshi.nakajima@gmail.com>
2020-08-27 13:58:14 -0700

    Initial Commit

---
+++ tablet/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  tablet
+//
+//  Created by SATOSHI NAKAJIMA on 8/27/20.
+//  Copyright © 2020 SATOSHI NAKAJIMA. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
